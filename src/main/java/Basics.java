9436e441c7db2cb3e22c555a939bc4876e8ad0a1
Michael Hui <michaelsjhui@gmail.com>
2022-09-10 00:10:39 -0400

    Completed TODOs 1-6

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -38,7 +38,7 @@ public class Basics {
          *
          *    (Relevant reading: 1.1.3. Printing things)
          */
-        System.out.println(7 + 5);
+        System.out.println("Hello World!");
 
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
@@ -62,7 +62,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
-
+        int my_variable = 100;
 
 
         /* Do not remove the line below: if you did task 2 correctly, then
@@ -100,6 +100,9 @@ public class Basics {
          * Current count: 0
          */
 
+        for (int i = 10; i > -1; i--){
+            System.out.println("Current count: " + i);
+        }
 
     }
 
@@ -141,6 +144,11 @@ public class Basics {
          *                you may use them if you wish.
          */
         StringBuilder ret = new StringBuilder();
+        String[] words = to_split.split(" ");
+
+        for (String word : words){
+            ret.append(word.charAt(0));
+        }
 
         // Fill in the rest of the body here
 
@@ -163,6 +171,10 @@ public class Basics {
      */
     public static int oddSum(int[] arr) {
         int current_sum = 0;
+
+        for (int i = 1; i < arr.length; i += 2){
+            current_sum += arr[i];
+        }
 
         /* TODO (Task 5): Complete this method body using a for-loop.
          *                You can find the length of an array by using the
